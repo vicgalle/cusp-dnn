08dcb081c63e194f23a88fca77a5ddb727ec705a
Victor Gallego <vicgalle@ucm.es>
2019-10-15 22:22:53 +0200

    start mgp

--- utils.py
+++ utils.py
@@ -9,3 +9,34 @@ def stick_breaking(alpha0, k):
     p = betas * remains
     p /= p.sum()
     return p
+
+def multiplicative_gamma(a1, a2, h):
+    """ Samples as in Eq (2) from [1] """
+    g1 = dist.Gamma(torch.tensor([a1]), torch.tensor([1.])).sample([1]).squeeze()
+    g2 = dist.Gamma(torch.tensor([a1]), torch.tensor([1.])).sample([h-1]).squeeze()
+
+    print(torch.cumprod( g2, dim=0))
+
+    taus = torch.cat((g1, torch.cumprod( g2, dim=0)), 0)
+
+    print(g1)
+    print(g2)
+    print(taus)
+
+multiplicative_gamma(.2, .2, 5)
+
+
+
+
+
+
+
+
+
+
+
+""" References
+
+[1] - Durante, D. "A note on the multiplicative gamma process". 2016.
+
+"""
